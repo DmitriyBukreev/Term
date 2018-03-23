102dd2222beb66cb09e40afdfd8b6d1c56fa14be
DmitriyBukreev <dmitribukree@yandex.ru>
2018-03-23 17:49:56 +0300

    Background launching implemented

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "precompiled.h"
 #define ARG_MAX 4096
 #define ARG_NUM 128
-#define EWRONGCOM 304
 
 // Splits the line into several strings with strtok
 // and makes the array of them
@@ -28,10 +27,12 @@ int main(int argn, char **argv)
 	char buf[ARG_MAX] = "";
 	char *args[ARG_NUM + 1];
 	pid_t pid;
+	int arg_len;
 	int stat_loc;
+	char wait_f;
 
 	while (1) {
-		printf("term: ");
+		printf("term: "); //Prompt
 		HANDLE_ERROR(fgets(buf, ARG_MAX, stdin), NULL);
 		if (buf[strlen(buf)-1] != '\n') {
 			printf("Error: The input line is too long\n");
@@ -43,16 +44,27 @@ int main(int argn, char **argv)
 			continue;
 		}
 
+		//Setting flag to enable or disable wait()
+		arg_len = strlen(args[0])-1;
+		if (args[0][arg_len] == '&') {
+			args[0][arg_len] = 0;
+			wait_f = 0;
+		} else
+			wait_f = 1;
+
 		pid = fork();
 		HANDLE_ERROR(pid, -1);
 		if (!pid) {
 			// Child
 			execvp(args[0], args);
 			// Error
-			printf("Сommand not found\n");
-			exit(EWRONGCOM);
+			perror("Error");
+			exit(errno);
 		}
 		// Parent
-		wait(&stat_loc);
+		if (wait_f)
+			wait(&stat_loc);
+		else
+			printf("[%i]\n", pid);
 	}
 }
--- src/precompiled.h
+++ src/precompiled.h
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
+#include <signal.h>
 #include <limits.h>
 #include <wait.h>
 #include <unistd.h>
